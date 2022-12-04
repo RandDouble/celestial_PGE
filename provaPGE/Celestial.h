0f1914e2300e6d7ae12d9d986e356aa147247c06
RandDouble <stefano.pilosio@gmail.com>
2022-12-04 18:13:55 +0100

    resolved some conflicts

--- provaPGE/Celestial.h
+++ provaPGE/Celestial.h
@@ -11,16 +11,12 @@ namespace Celestial {
 	{
 	protected:
 		const double m_mass{ 1. };
-	
-
-		
-
 		//std::vector<olc::vd2d> operator()(float fElapsedTime, std::vector<olc::vd2d> choords) override;
 
 	public:
 		Celestial(olc::PixelGameEngine* game) : Balls(game) {}
 		Celestial(olc::vd2d pos, olc::vd2d vel, double mass) : Balls(pos, vel), m_mass(mass) {}
-	
+
 		void ChangePos(olc::PixelGameEngine* game, float fElapsedTime);
 
 		//std::vector<olc::vd2d> Choords() const override;
--- provaPGE/VectorOp.h
+++ provaPGE/VectorOp.h
@@ -8,7 +8,7 @@
 #include <assert.h>
 
 
-template <class T, class Y> std::vector <T> operator* (const std::vector<T>& v,const  Y& k)
+template <class T, class Y> std::vector <T> operator* (const std::vector<T>& v, const  Y& k)
 {
 	std::vector<T> res;
 	//std::transform(v.cbegin(), v.cend(), res.begin(), [&k](T a){return a * k; });
@@ -25,7 +25,7 @@ template <class T, class Y> std::vector <T> operator* (const Y& k, const std::ve
 }
 
 
-template<class T> T operator* (const std::vector<T>& lhs,const std::vector<T>& rhs)
+template<class T> T operator* (const std::vector<T>& lhs, const std::vector<T>& rhs)
 {
 	T res = 0;
 	assert(lhs.size() != rhs.size() && "operand doesn't match in size");
@@ -33,7 +33,7 @@ template<class T> T operator* (const std::vector<T>& lhs,const std::vector<T>& r
 	return res;
 }
 
-template <class T> std::vector <T> operator+ (const std::vector<T>& A,const std::vector<T>& B)
+template <class T> std::vector <T> operator+ (const std::vector<T>& A, const std::vector<T>& B)
 {
 	std::vector<T> res;
 	//std::transform(A.cbegin(), A.cend(), B.cbegin(), res.begin(), std::plus<>{});
