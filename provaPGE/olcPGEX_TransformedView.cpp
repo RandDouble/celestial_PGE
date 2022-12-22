855647877441433755d74522e24402e158e29121
RandDouble <stefano.pilosio@gmail.com>
2022-12-22 23:14:20 +0100

    Resolved Linking problems

---
+++ provaPGE/olcPGEX_TransformedView.cpp
@@ -0,0 +1,2 @@
+#define OLC_PGEX_TRANSFORMEDVIEW
+#include "incs/olcPGEX_TransformedView.h"
--- provaPGE/src/olcPixelGameEngine.cpp
+++ provaPGE/src/olcPixelGameEngine.cpp
@@ -1,3 +1,2 @@
 #define OLC_PGE_APPLICATION
-#define OLC_PGEX_TRANSFORMEDVIEW
 #include "../incs/olcPixelGameEngine.h"
